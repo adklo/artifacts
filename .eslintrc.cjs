27743111d779a9d70ec40e51070da139266765df
Adrian <an.klodzinski@gmail.com>
2026-01-29 11:10:56 +0100

    Changed eslint config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,17 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es2022: true
+  },
   rules: {
     semi: "error",
-    "prefer-const": "error",
-  },
+    "prefer-const": "error"
+  }
 };
